796aa101e46a7b2b1dfd2d552e3f0b82d7127f22
DEvitor77 <valtervitorfilho@gmail.com>
2024-07-14 23:24:53 -0300

    metodos post e get funcionando

--- server.js
+++ server.js
@@ -1,19 +1,30 @@
 import express, { request } from 'express'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 const app = express()
 app.use(express.json())
 
-const users = []
 
-app.post('/usuarios', (req, res) => {
+app.post('/usuarios', async (req, res) => {
 
-    users.push(req.body)
+    await prisma.user.create({
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+        }
+    })
 
-    res.send('ok post deu certo')
+    res.status(201).json(req.body)
 })
 
-app.get('/usuarios', (req, res) => {
-    res.json(users)
+app.get('/usuarios', async (req, res) => {
+
+    const users = await prisma.user.findMany()
+
+    res.status(200).json(users)
 })
 
 app.listen(3000)
