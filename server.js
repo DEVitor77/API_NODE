bba9843839ad0d71871d4fd185927f783f8bb2cf
DEvitor77 <valtervitorfilho@gmail.com>
2024-07-14 22:17:20 -0300

    Criando api em crud

---
+++ server.js
@@ -0,0 +1,24 @@
+import express, { request } from 'express'
+
+const app = express()
+app.use(express.json())
+
+const users = []
+
+app.post('/usuarios', (req, res) => {
+
+    users.push(req.body)
+
+    res.send('ok post deu certo')
+})
+
+app.get('/usuarios', (req, res) => {
+    res.json(users)
+})
+
+app.listen(3000)
+
+/*
+    1) Tipo de rota/  metodo HTTP
+    2) ENDERECO
+*/
